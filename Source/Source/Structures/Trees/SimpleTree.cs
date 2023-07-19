ee4aa8fcb39563f764cdfc980db57cd86f05c3f5
bookWorm21 <ilyagolosovoice@gmail.com>
2023-07-19 03:05:41 +0300

    Simple tree realization (without collision)

---
+++ Source/Source/Structures/Trees/SimpleTree.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class SimpleTreeNode<T>
+    {
+        public T NodeValue;
+        public SimpleTreeNode<T> Parent;
+        public List<SimpleTreeNode<T>> Children;
+	
+        public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
+        {
+            NodeValue = val;
+            Parent = parent;
+            Children = new List<SimpleTreeNode<T>>();
+        }
+    }
+	
+    public class SimpleTree<T>
+    {
+        public SimpleTreeNode<T> Root;
+
+        public SimpleTree(SimpleTreeNode<T> root)
+        {
+            Root = root;
+        }
+	
+        public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+        {
+            if (ParentNode == null)
+            {
+                Root = NewChild;
+                NewChild.Parent = null;
+                return;
+            }
+
+            NewChild.Parent = ParentNode;
+            ParentNode.Children.Add(NewChild);
+        }
+
+        public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
+        {
+            if (NodeToDelete.Parent == null)
+            {
+                Root = null;
+                return;
+            }
+            
+            NodeToDelete.Parent.Children.Remove(NodeToDelete);
+            NodeToDelete.Parent = null;
+        }
+
+        public List<SimpleTreeNode<T>> GetAllNodes()
+        {
+            var result = new List<SimpleTreeNode<T>>();
+            RecursiveMakingNodes(Root);
+            return result;
+
+            void RecursiveMakingNodes(SimpleTreeNode<T> node)
+            {
+                if (node == null)
+                    return;
+
+                result.Add(node);
+
+                foreach (var child in node.Children)
+                {
+                    RecursiveMakingNodes(child);
+                }
+            }
+        }
+	
+        public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+        {
+            var result = new List<SimpleTreeNode<T>>();
+            RecursiveFinding(Root);
+            return result;
+
+            void RecursiveFinding(SimpleTreeNode<T> node)
+            {
+                if (node == null)
+                    return;
+
+                if (node.NodeValue.Equals(val))
+                {
+                    result.Add(node);
+                }
+
+                foreach (var child in node.Children)
+                {
+                    RecursiveFinding(child);
+                }
+            }
+        }
+   
+        public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+        {
+            DeleteNode(OriginalNode);
+            AddChild(NewParent, OriginalNode);
+        }
+   
+        public int Count()
+        {
+            int count = 0;
+            RecursiveCounting(Root);
+            return count;
+
+            void RecursiveCounting(SimpleTreeNode<T> node)
+            {
+                if (node == null)
+                    return;
+
+                ++count;
+                foreach (var child in node.Children)
+                {
+                    RecursiveCounting(child);
+                }
+            }
+        }
+
+        public int LeafCount()
+        {
+            int count = 0;
+            RecursiveCounting(Root);
+            return count;
+
+            void RecursiveCounting(SimpleTreeNode<T> node)
+            {
+                if (node == null)
+                    return;
+
+                if (node.Children.Count == 0)
+                {
+                    ++count;
+                }
+
+                foreach (var child in node.Children)
+                {
+                    RecursiveCounting(child);
+                }
+            }
+        }
+    }
+}
---
+++ Source/UnitTests/Structures/Trees/SimpleTreeTests.cs
@@ -0,0 +1,196 @@
+﻿using System.Linq;
+using AlgorithmsDataStructures2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Structures.Trees
+{
+    [TestClass]
+    public class SimpleTreeTests
+    {
+        [TestMethod]
+        public void AddChild_ToEmptyTree()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            tree.AddChild(null, node);
+
+            Assert.AreEqual(node, tree.Root);
+            Assert.IsTrue(tree.Root.Parent == null);
+            Assert.IsTrue(tree.Root.Children != null && tree.Root.Children.Count == 0);
+        }
+
+        [TestMethod]
+        public void AddChild_TwoNodes()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(10, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+
+            Assert.IsTrue(tree.Root == node);
+            Assert.IsTrue(tree.Root.Children.Contains(node2));
+            Assert.IsTrue(node2.Parent == node);
+        }
+
+        [TestMethod]
+        public void DeleteNode_RootWithChild()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(10, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+
+            tree.DeleteNode(node);
+
+            Assert.IsTrue(tree.Root == null);
+        }
+        
+        [TestMethod]
+        public void DeleteNode_ChildOnRoot()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(10, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+
+            tree.DeleteNode(node2);
+
+            Assert.IsTrue(tree.Root == node);
+            Assert.IsFalse(tree.Root.Children.Contains(node2));
+        }
+
+        [TestMethod]
+        public void MoveNode_1()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(5, null);
+            var node3 = new SimpleTreeNode<int>(8, null);
+            var node4 = new SimpleTreeNode<int>(5, null);
+            var node5 = new SimpleTreeNode<int>(10, null);
+            var node6 = new SimpleTreeNode<int>(51, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+            tree.AddChild(node2, node3);
+            tree.AddChild(node2, node4);
+            tree.AddChild(node, node5);
+            tree.AddChild(node5, node6);
+
+            tree.MoveNode(node2, node5);
+
+            Assert.IsFalse(node.Children.Contains(node2));
+            Assert.IsTrue(node2.Parent == node5);
+            Assert.IsTrue(node5.Children.Contains(node2));
+            Assert.IsTrue(node5.Children.Contains(node6));
+        }
+
+        [TestMethod]
+        public void GetAllNodes_BambooTreeType()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(5, null);
+            var node3 = new SimpleTreeNode<int>(8, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+            tree.AddChild(node2, node3);
+
+            var actual = tree.GetAllNodes();
+
+            Assert.IsTrue(actual.Contains(node));
+            Assert.IsTrue(actual.Contains(node2));
+            Assert.IsTrue(actual.Contains(node3));
+        }
+
+        [TestMethod]
+        public void FindNodesByValue_SeveralNodesWithSameValues()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(5, null);
+            var node3 = new SimpleTreeNode<int>(8, null);
+            var node4 = new SimpleTreeNode<int>(5, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+            tree.AddChild(node2, node3);
+            tree.AddChild(node2, node4);
+
+            var actual = tree.FindNodesByValue(5);
+
+            Assert.AreEqual(3, actual.Count);
+            Assert.IsTrue(actual.Contains(node));
+            Assert.IsTrue(actual.Contains(node2));
+            Assert.IsTrue(actual.Contains(node4));
+        }
+        
+        [TestMethod]
+        public void Count_1()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(5, null);
+            var node3 = new SimpleTreeNode<int>(8, null);
+            var node4 = new SimpleTreeNode<int>(5, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+            tree.AddChild(node2, node3);
+            tree.AddChild(node2, node4);
+
+            Assert.AreEqual(4, tree.Count());
+        }
+        
+        [TestMethod]
+        public void LeafCount_1()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+
+            var node = new SimpleTreeNode<int>(5, null);
+            var node2 = new SimpleTreeNode<int>(5, null);
+            var node3 = new SimpleTreeNode<int>(8, null);
+            var node4 = new SimpleTreeNode<int>(5, null);
+
+            tree.AddChild(null, node);
+            tree.AddChild(node, node2);
+            tree.AddChild(node2, node3);
+            tree.AddChild(node2, node4);
+
+            Assert.AreEqual(2, tree.LeafCount());
+        }
+        
+        [TestMethod]
+        [Description("Empty tree")]
+        public void LeafCount_2()
+        {
+            var tree = new SimpleTree<int>(null);
+            Assert.AreEqual(null, tree.Root);
+            Assert.AreEqual(0, tree.LeafCount());
+        }
+    }
+}
