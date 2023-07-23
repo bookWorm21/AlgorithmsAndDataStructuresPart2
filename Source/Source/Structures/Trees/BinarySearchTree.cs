32ae491c21adf38c2af15b29d27234775b6a3dcc
bookWorm21 <ilyagolosovoice@gmail.com>
2023-07-23 15:46:07 +0300

    binary search tree without balance realization

---
+++ Source/Source/Structures/Trees/BinarySearchTree.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class BSTNode<T>
+    {
+        public int NodeKey;
+        public T NodeValue;
+        public BSTNode<T> Parent;
+        public BSTNode<T> LeftChild;
+        public BSTNode<T> RightChild;
+	
+        public BSTNode(int key, T val, BSTNode<T> parent)
+        {
+            NodeKey = key;
+            NodeValue = val;
+            Parent = parent;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+    
+    public class BSTFind<T>
+    {
+        public BSTNode<T> Node;
+        
+        public bool NodeHasKey;
+        
+        public bool ToLeft;
+
+        public BSTFind()
+        {
+            Node = null;
+        }
+    }
+
+    public class BST<T>
+    {
+        BSTNode<T> Root;
+	
+        public BST(BSTNode<T> node)
+        {
+            Root = node;
+        }
+	
+        public BSTFind<T> FindNodeByKey(int key)
+        {
+            if (Root == null)
+                return new BSTFind<T>();
+
+            return FindNode(Root);
+
+            BSTFind<T> FindNode(BSTNode<T> node)
+            {
+                if (node.NodeKey == key)
+                {
+                    return new BSTFind<T>
+                    {
+                        Node = node,
+                        NodeHasKey = true,
+                        ToLeft = node.Parent != null && node.Parent.LeftChild == node
+                    };
+                }
+
+                if (key < node.NodeKey && node.LeftChild != null)
+                    return FindNode(node.LeftChild);
+
+                if (key < node.NodeKey && node.LeftChild == null)
+                    return new BSTFind<T>
+                    {
+                        Node = node,
+                        NodeHasKey =  false,
+                        ToLeft =  true
+                    };
+
+                if (key > node.NodeKey && node.RightChild != null)
+                    return FindNode(node.RightChild);
+
+                if (key > node.NodeKey && node.RightChild == null)
+                    return new BSTFind<T>
+                    {
+                        Node = node,
+                        NodeHasKey = false,
+                        ToLeft = false
+                    };
+
+                return new BSTFind<T>();
+            }
+        }
+
+        public bool AddNode(BSTNode<T> addNode)
+        {
+            var finded = FindNodeByKey(addNode.NodeKey);
+
+            if (finded.Node == null)
+            {
+                addNode.Parent = null;
+                Root = addNode;
+                return true;
+            }
+            
+            if (finded.NodeHasKey)
+            {
+                return false;
+            }
+
+            if (finded.ToLeft)
+            {
+                addNode.Parent = finded.Node;
+                finded.Node.LeftChild = addNode;
+            }
+            else
+            {
+                addNode.Parent = finded.Node;
+                finded.Node.RightChild = addNode;
+            }
+
+            return true;
+        }
+        
+        public bool AddKeyValue(int key, T val)
+        {
+            var finded = FindNodeByKey(key);
+
+            if (finded.Node == null)
+            {
+                Root = new BSTNode<T>(key, val, null);
+                return true;
+            }
+            
+            if (finded.NodeHasKey)
+            {
+                return false;
+            }
+
+            if (finded.ToLeft)
+            {
+                finded.Node.LeftChild = new BSTNode<T>(key, val, finded.Node);
+            }
+            else
+            {
+                finded.Node.RightChild = new BSTNode<T>(key, val, finded.Node);
+            }
+
+            return true;
+        }
+	
+        public BSTNode<T> FinMinMax(BSTNode<T> FromNode, bool FindMax)
+        {
+            return FindMax ? FindMaxNode(FromNode) : FindMinNode(FromNode);
+
+            BSTNode<T> FindMaxNode(BSTNode<T> node)
+            {
+                if (node == null)
+                    return null;
+                
+                while (true)
+                {
+                    if (node.RightChild == null)
+                        return node;
+
+                    node = node.RightChild;
+                }
+            }
+
+            BSTNode<T> FindMinNode(BSTNode<T> node)
+            {
+                if (node == null)
+                    return null;
+
+                while (true)
+                {
+                    if (node.LeftChild == null)
+                        return node;
+
+                    node = node.LeftChild;
+                }
+            }
+        }
+	
+        public bool DeleteNodeByKey(int key)
+        {
+            var finded = FindNodeByKey(key);
+
+            if (!finded.NodeHasKey)
+            {
+                return false;
+            }
+
+            if (finded.Node.Parent == null)
+            {
+                Root = null;
+                return true;
+            }
+
+            if (finded.Node.LeftChild == null && finded.Node.RightChild == null && finded.ToLeft)
+            {
+                finded.Node.Parent.LeftChild = null;
+                return true;
+            }
+
+            if (finded.Node.LeftChild == null && finded.Node.RightChild == null)
+            {
+                finded.Node.Parent.RightChild = null;
+                return true;
+            }
+
+            if (finded.Node.LeftChild == null && finded.ToLeft)
+            {
+                finded.Node.RightChild.Parent = finded.Node.Parent;
+                finded.Node.Parent.LeftChild = finded.Node.RightChild;
+
+                return true;
+            }
+
+            if (finded.Node.LeftChild == null)
+            {
+                finded.Node.RightChild.Parent = finded.Node.Parent;
+                finded.Node.Parent.RightChild = finded.Node.RightChild;
+                
+                return true;
+            }
+
+            if (finded.Node.RightChild == null && finded.ToLeft)
+            {
+                finded.Node.LeftChild.Parent = finded.Node.Parent;
+                finded.Node.Parent.LeftChild = finded.Node.LeftChild;
+
+                return true;
+            }
+
+            if (finded.Node.RightChild == null)
+            {
+                finded.Node.LeftChild.Parent = finded.Node.Parent;
+                finded.Node.Parent.RightChild = finded.Node.LeftChild;
+                
+                return true;
+            }
+
+            var minOnRight = FinMinMax(finded.Node.RightChild, false);
+            
+            if (finded.ToLeft)
+            {
+                finded.Node.Parent.LeftChild = minOnRight;
+            }
+            else
+            {
+                finded.Node.Parent.RightChild = minOnRight;
+            }
+
+            if (minOnRight.Parent.LeftChild == minOnRight)
+            {
+                minOnRight.Parent.LeftChild = null;
+            }
+            else
+            {
+                minOnRight.Parent.RightChild = null;
+            }
+            
+            minOnRight.Parent = finded.Node.Parent;
+            minOnRight.LeftChild = finded.Node.LeftChild;
+            minOnRight.RightChild = finded.Node.RightChild;
+            minOnRight.LeftChild.Parent = minOnRight;
+            minOnRight.RightChild.Parent = minOnRight;
+
+            return true;
+        }
+
+        public int Count()
+        {
+            var count = 0;
+            RecursiveCounting(Root);
+            return count;
+
+            void RecursiveCounting(BSTNode<T> node)
+            {
+                if (node == null)
+                    return;
+
+                ++count;
+
+                RecursiveCounting(node.LeftChild);
+                RecursiveCounting(node.RightChild);
+            }
+        }
+    }
+}
---
+++ Source/UnitTests/Structures/Trees/BinarySearchTreeTests.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsDataStructures2;
+
+namespace UnitTests.Structures.Trees
+{
+    [TestClass]
+    public class BinarySearchTreeTests
+    {
+        [TestMethod]
+        public void AddKeyToEmptyTree()
+        {
+            var tree = new BST<int>(null);
+
+            Assert.AreEqual(true, tree.AddKeyValue(5, 10));
+            Assert.AreEqual(false, tree.AddKeyValue(5, 10));
+
+            Assert.AreEqual(true, tree.AddKeyValue(7, 10));
+            Assert.AreEqual(true, tree.AddKeyValue(4, 10));
+        }
+
+        [TestMethod]
+        public void CountAfterAddedToEmptyTree()
+        {
+            var tree = new BST<int>(null);
+
+            Assert.AreEqual(0, tree.Count());
+            
+            tree.AddKeyValue(5, 10);
+            tree.AddKeyValue(6, 10);
+            tree.AddKeyValue(4, 10);
+            tree.AddKeyValue(10, 10);
+            tree.AddKeyValue(11, 10);
+
+            Assert.AreEqual(5, tree.Count());
+        }
+
+        [TestMethod]
+        public void FindMinMaxTest1()
+        {
+            var root = new BSTNode<int>(5, 8, null);
+            var node1 = new BSTNode<int>(6, 9, null);
+            var node2 = new BSTNode<int>(4, 10, null);
+            var node3 = new BSTNode<int>(10, 11, null);
+            
+            var tree = new BST<int>(null);
+
+            Assert.AreEqual(null, tree.FinMinMax(null, true));
+            Assert.AreEqual(null, tree.FinMinMax(null, false));
+
+            tree.AddNode(root);
+            tree.AddNode(node1);
+            tree.AddNode(node2);
+            tree.AddNode(node3);
+
+            Assert.AreEqual(node3, tree.FinMinMax(root, true));
+            Assert.AreEqual(node2, tree.FinMinMax(root, false));
+            Assert.AreEqual(node2, tree.FinMinMax(node2, true));
+            Assert.AreEqual(node2, tree.FinMinMax(node2, false));
+            Assert.AreEqual(node3, tree.FinMinMax(node1, true));
+        }
+
+        [TestMethod]
+        public void FindNodeByKeyCornel()
+        {
+            var root = new BSTNode<int>(5, 8, null);
+
+            var tree = new BST<int>(null);
+
+            var finded = tree.FindNodeByKey(5);
+            
+            Assert.IsTrue(finded.Node == null);
+            Assert.IsFalse(finded.NodeHasKey);
+
+            tree.AddNode(root);
+
+            finded = tree.FindNodeByKey(5);
+            Assert.IsTrue(finded.Node == root);
+            Assert.IsTrue(finded.NodeHasKey);
+
+            finded = tree.FindNodeByKey(8);
+
+            Assert.IsTrue(finded.Node == root);
+            Assert.IsFalse(finded.NodeHasKey);
+            Assert.IsFalse(finded.ToLeft);
+
+            finded = tree.FindNodeByKey(4);
+
+            Assert.IsTrue(finded.Node == root);
+            Assert.IsFalse(finded.NodeHasKey);
+            Assert.IsTrue(finded.ToLeft);
+        }
+
+        [TestMethod]
+        public void DeleteNodeByKeyCornel()
+        {
+            var tree = new BST<int>(null);
+
+            Assert.AreEqual(false, tree.DeleteNodeByKey(5));
+            
+            var root20 = new BSTNode<int>(20, 8, null);
+            var node10 = new BSTNode<int>(10, 8, null);
+            var node30 = new BSTNode<int>(30, 8, null);
+            var node5 = new BSTNode<int>(5, 8, null);
+            var node15 = new BSTNode<int>(15, 8, null);
+            var node0 = new BSTNode<int>(0, 8, null);
+            var node8 = new BSTNode<int>(8, 8, null);
+            var node12 = new BSTNode<int>(12, 8, null);
+            var node17 = new BSTNode<int>(17, 8, null);
+            var node25 = new BSTNode<int>(25, 8, null);
+            var node22 = new BSTNode<int>(22, 8, null);
+            var node27 = new BSTNode<int>(27, 8, null);
+            var node35 = new BSTNode<int>(35, 8, null);
+            
+            tree.AddNode(root20);
+            tree.AddNode(node10);
+            tree.AddNode(node5);
+            tree.AddNode(node8);
+            tree.AddNode(node0);
+            tree.AddNode(node15);
+            tree.AddNode(node17);
+            tree.AddNode(node12);
+            tree.AddNode(node30);
+            tree.AddNode(node35);
+            tree.AddNode(node25);
+            tree.AddNode(node27);
+            tree.AddNode(node22);
+
+            Assert.IsTrue(tree.DeleteNodeByKey(35));
+            Assert.IsTrue(node30.RightChild == null);
+
+            Assert.IsTrue(tree.DeleteNodeByKey(27));
+            Assert.IsTrue(node25.RightChild == null);
+
+            Assert.IsTrue(tree.DeleteNodeByKey(25));
+            Assert.IsTrue(node30.LeftChild == node22);
+
+            Assert.IsTrue(tree.DeleteNodeByKey(10));
+            Assert.IsTrue(root20.LeftChild == node12);
+            Assert.IsTrue(node12.Parent == root20);
+            Assert.IsTrue(node5.Parent == node12);
+            Assert.IsTrue(node15.Parent == node12);
+            Assert.IsTrue(node15.LeftChild == null);
+            Assert.IsTrue(node12.LeftChild == node5);
+            Assert.IsTrue(node12.RightChild == node15);
+        }
+    }
+}
