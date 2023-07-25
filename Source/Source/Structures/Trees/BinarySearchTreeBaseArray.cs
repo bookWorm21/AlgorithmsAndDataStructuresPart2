1107c45d1ce815c3642fbc2c1e99b634e0f6b99b
bookWorm21 <ilyagolosovoice@gmail.com>
2023-07-26 02:23:15 +0300

    binary search tree base array realization

---
+++ Source/Source/Structures/Trees/BinarySearchTreeBaseArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class aBST
+    {
+        public int? [] Tree;
+
+        public aBST(int depth)
+        {
+            var treeSize = (int)Math.Pow(2, depth) - 1;
+            Tree = new int?[treeSize];
+            for (var i = 0; i < treeSize; i++) 
+                Tree[i] = null;
+        }
+
+        public int? FindKeyIndex(int key)
+        {
+            return RecursiveFind(0);
+
+            int? RecursiveFind(int index)
+            {
+                while (true)
+                {
+                    if (index >= Tree.Length) 
+                        return null;
+
+                    if (Tree[index] == key) 
+                        return index;
+
+                    if (Tree[index] == null) 
+                        return -index;
+
+                    if (key > Tree[index])
+                    {
+                        index = index * 2 + 2;
+                        continue;
+                    }
+
+                    index = index * 2 + 1;
+                }
+            }
+        }
+	
+        public int AddKey(int key)
+        {
+            var finded = FindKeyIndex(key);
+            if (finded == null)
+                return -1;
+
+            int index;
+            if (finded < 0)
+            {
+                index = (int) -finded;
+                Tree[index] = key;
+                return index;
+            }
+
+            index = (int) finded;
+
+            if (Tree[0] == null && index == 0)
+            {
+                Tree[0] = key;
+            }
+            
+            return index;
+        }
+	
+    }
+}
---
+++ Source/UnitTests/Structures/Trees/BinarySearchTreeBaseArrayTests.cs
@@ -0,0 +1,67 @@
+﻿using AlgorithmsDataStructures2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Structures.Trees
+{
+    [TestClass]
+    public class BinarySearchTreeBaseArrayTests
+    {
+        [TestMethod]
+        public void FindKeyIndexCornel()
+        {
+            var tree = new aBST(2);
+
+            var finded = tree.FindKeyIndex(8);
+
+            Assert.IsFalse(finded == null);
+            Assert.IsTrue((int) finded == 0);
+
+            tree.Tree[0] = 8;
+
+            finded = tree.FindKeyIndex(8);
+
+            Assert.IsFalse(finded == null);
+            Assert.IsTrue((int) finded == 0);
+
+            finded = tree.FindKeyIndex(10);
+
+            Assert.IsFalse(finded == null);
+            Assert.IsTrue((int) finded == -2);
+
+            finded = tree.FindKeyIndex(0);
+
+            Assert.IsFalse(finded == null);
+            Assert.IsTrue((int) finded == -1);
+
+            tree.Tree[2] = 9;
+
+            finded = tree.FindKeyIndex(9);
+            
+            Assert.IsFalse(finded == null);
+            Assert.AreEqual(2, (int) finded);
+
+            finded = tree.FindKeyIndex(10);
+
+            Assert.IsTrue(finded == null);
+        }
+
+        [TestMethod]
+        public void AddKeyCornel()
+        {
+            var tree = new aBST(2);
+
+            var addResult = tree.AddKey(20);
+            Assert.AreEqual(0, addResult);
+            Assert.AreEqual(20, tree.Tree[0]);
+
+            addResult = tree.AddKey(20);
+            Assert.AreEqual(0, addResult);
+
+            addResult = tree.AddKey(10);
+            Assert.AreEqual(1, addResult);
+
+            addResult = tree.AddKey(8);
+            Assert.AreEqual(-1, addResult);
+        }
+    }
+}
