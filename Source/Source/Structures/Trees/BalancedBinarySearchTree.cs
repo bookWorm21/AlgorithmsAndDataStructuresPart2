57790d083a50670533478db90d12cc02696faee3
bookWorm21 <ilyagolosovoice@gmail.com>
2023-07-29 21:46:04 +0300

    balanced array by binary search tree rool

---
+++ Source/Source/Structures/Trees/BalancedBinarySearchTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            Array.Sort(a);
+            var result = new int[a.Length];
+            SetValues(0, result.Length - 1, 0);
+            return result;
+
+            void SetValues(int left, int right, int index)
+            {
+                if (left > right)
+                    return;
+                
+                var middle = left + (right - left) / 2;
+                result[index] = a[middle];
+
+                SetValues(left, middle - 1, index * 2 + 1);
+                SetValues(middle + 1, right, index * 2 + 2);
+            }
+        }
+    }
+}
---
+++ Source/UnitTests/Structures/Trees/BalancedBinarySearchTreeTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsDataStructures2;
+
+namespace UnitTests.Structures.Trees
+{
+    [TestClass]
+    public class BalancedBinarySearchTreeTests
+    {
+        [TestMethod]
+        public void GenerateBBSTArray()
+        {
+            var empty = Array.Empty<int>();
+            Assert.IsTrue(Array.Empty<int>().SequenceEqual(BalancedBST.GenerateBBSTArray(empty)));
+            
+            var withOneElement = new int[1];
+            withOneElement[0] = 121;
+            
+            Assert.IsTrue(new List<int> {121}.ToArray().SequenceEqual(BalancedBST.GenerateBBSTArray(withOneElement)));
+            
+            var withTwoElement = new int[3] {120, 130, 100};
+            Assert.IsTrue(new List<int>{120, 100, 130}.SequenceEqual(BalancedBST.GenerateBBSTArray(withTwoElement)));
+            
+            //                     20 
+            //           10                  30
+            //         5    15         25          35
+            //        0 8  12 17      22 27     33    40
+            var treeElements = new[] {10, 5, 15, 0, 8, 12, 17, 20, 25, 30, 35, 22, 27, 40, 33};
+            var expected = new[] {20, 10, 30, 5, 15, 25, 35, 0, 8, 12, 17, 22, 27, 33, 40};
+
+            Assert.IsTrue(expected.SequenceEqual(BalancedBST.GenerateBBSTArray(treeElements)));
+        }
+    }
+}
