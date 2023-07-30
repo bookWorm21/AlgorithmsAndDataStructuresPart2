c2cd84620d6435d0219c7fbed88512b670de04b1
bookWorm21 <ilyagolosovoice@gmail.com>
2023-07-30 16:15:53 +0300

    heap realization

---
+++ Source/Source/Structures/Trees/Heap.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Heap
+    {
+        public int [] HeapArray;
+        public int Size { get; private set; }
+
+        public Heap()
+        {
+            HeapArray = null;
+            Size = 0;
+        }
+		
+        public void MakeHeap(int[] a, int depth)
+        {
+            Size = a.Length;
+            var heapSize = (int)Math.Pow(2, depth + 1) - 1;
+            HeapArray = new int[heapSize];
+            Array.Sort(a);
+            Array.Reverse(a);
+
+            for (var i = 0; i < a.Length; ++i)
+            {
+                HeapArray[i] = a[i];
+            }
+
+            for (var i = a.Length; i < HeapArray.Length; ++i)
+            {
+                HeapArray[i] = -1;
+            }
+        }
+		
+        public int GetMax()
+        {
+            if (Size == 0)
+                return -1;
+
+            var max = HeapArray[0];
+            HeapArray[0] = HeapArray[Size - 1];
+            HeapArray[Size - 1] = -1;
+            --Size;
+            PropagateToBottom(0);
+            return max;
+        }
+
+        public bool Add(int key)
+        {
+            if (Size == HeapArray.Length)
+                return false;
+            
+            HeapArray[Size] = key;
+            ++Size;
+            PropagateToTop(Size - 1);
+            return true;
+        }
+        
+        private void PropagateToTop(int index)
+        {
+            if (index == 0)
+                return;
+
+            var parent = (index - 1) / 2;
+            var left = parent * 2 + 1;
+            var right = parent * 2 + 2;
+
+            var maxIndex = left;
+            if (right < Size && HeapArray[right] > HeapArray[left])
+            {
+                maxIndex = right;
+            }
+
+            if (HeapArray[maxIndex] > HeapArray[parent])
+            {
+                (HeapArray[maxIndex], HeapArray[parent]) = (HeapArray[parent], HeapArray[maxIndex]);
+                PropagateToTop(parent);
+            }
+        }
+
+        private void PropagateToBottom(int index)
+        {
+            var left = index * 2 + 1;
+            var right = index * 2 + 2;
+
+            if (left >= Size)
+            {
+                return;
+            }
+
+            var maxIndex = left;
+            if (right < Size && HeapArray[right] > HeapArray[left])
+            {
+                maxIndex = right;
+            }
+            
+            if (HeapArray[maxIndex] > HeapArray[index])
+            {
+                (HeapArray[maxIndex], HeapArray[index]) = (HeapArray[index], HeapArray[maxIndex]);
+                PropagateToBottom(maxIndex);
+            }
+        }
+    }
+}
---
+++ Source/UnitTests/Structures/Trees/HeapTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmsDataStructures2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Structures.Trees
+{
+    [TestClass]
+    public class HeapTests
+    {
+        [TestMethod]
+        public void MakeHeapOneElement()
+        {
+            var heap = new Heap();
+            heap.MakeHeap(new List<int> {5}.ToArray(), 1);
+
+            Assert.IsTrue(heap.HeapArray[0] == 5);
+            Assert.IsTrue(heap.Size == 1);
+        }
+        
+        [TestMethod]
+        public void MakeHeapOrderTest()
+        {
+            var random = new Random(999);
+            var depth = 4;
+            var count = 13;
+
+            var values = new int[count];
+            for (int i = 0; i < values.Length; ++i)
+            {
+                values[i] = random.Next(1, 100);
+            }
+            
+            var heap = new Heap();
+            heap.MakeHeap(values, depth);
+            var length = heap.Size;
+
+            Check(0);
+
+            void Check(int index)
+            {
+                if (index >= length)
+                    return;
+
+                var left = index * 2 + 1;
+                var right = index * 2 + 2;
+
+                Assert.IsTrue(heap.HeapArray[index] >= heap.HeapArray[left]);
+                Assert.IsTrue(heap.HeapArray[index] >= heap.HeapArray[right]);
+
+                Check(left);
+                Check(right);
+            }
+        }
+
+        [TestMethod]
+        public void GetMaxOrderTest()
+        {            var random = new Random(999);
+            var depth = 4;
+            var count = 13;
+
+            var values = new int[count];
+            for (int i = 0; i < values.Length; ++i)
+            {
+                values[i] = random.Next(1, 100);
+            }
+            
+            var heap = new Heap();
+            heap.MakeHeap(values, depth);
+
+            Array.Sort(values);
+            Array.Reverse(values);
+            
+            for (int i = 0; i < values.Length; ++i)
+            {
+                Assert.AreEqual(values[i], heap.GetMax());
+                Assert.AreEqual(values.Length - i - 1, heap.Size);
+            }
+
+            Assert.AreEqual(-1, heap.GetMax());
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var random = new Random(999);
+            var depth = 4;
+
+            var values = Array.Empty<int>();
+
+            var heap = new Heap();
+            heap.MakeHeap(values, depth);
+
+            while (heap.Add(random.Next(1, 100)))
+            {
+            }
+            
+            var length = heap.Size;
+
+            Assert.AreEqual((int) Math.Pow(2, depth + 1) - 1, length);
+
+            Check(0);
+
+            void Check(int index)
+            {
+                if (index >= length)
+                    return;
+                
+                var parent = (index - 1) / 2;
+
+                if (parent >= 0)
+                {
+                    Assert.IsTrue(heap.HeapArray[parent] >= heap.HeapArray[index]);
+                }
+                
+                var left = index * 2 + 1;
+                var right = index * 2 + 2;
+
+                Check(left);
+                Check(right);
+            }
+        }
+    }
+}
