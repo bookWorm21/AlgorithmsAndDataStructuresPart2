1cc4081d96b062f7ad9dc95f883264d59825ca7a
bookWorm21 <ilyagolosovoice@gmail.com>
2023-08-05 19:05:49 +0300

    simple graph with adjacency matrix realization

---
+++ Source/Source/Structures/SimpleGraph.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Vertex
+    {
+        public int Value;
+        public Vertex(int val)
+        {
+            Value = val;
+        }
+    }
+  
+    public class SimpleGraph
+    {
+        public Vertex [] vertex;
+        public int [,] m_adjacency;
+        public int max_vertex;
+
+        public int FreeIndex { get; private set; }
+
+        public SimpleGraph(int size)
+        {
+            max_vertex = size;
+            m_adjacency = new int [size,size];
+            vertex = new Vertex [size];
+        }
+	
+        public void AddVertex(int value)
+        {
+            if (FreeIndex >= max_vertex)
+            {
+                return;
+            }
+
+            vertex[FreeIndex] = new Vertex(value);
+            ++FreeIndex;
+        }
+        
+        public void RemoveVertex(int v)
+        {
+            vertex[v] = null;
+
+            for (var i = 0; i < FreeIndex; ++i)
+            {
+                m_adjacency[i, v] = 0;
+            }
+
+            --FreeIndex;
+        }
+	
+        public bool IsEdge(int v1, int v2)
+        {
+            return m_adjacency[v1, v2] == 1;
+        }
+	
+        public void AddEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 1;
+            m_adjacency[v2, v1] = 1;
+        }
+	
+        public void RemoveEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 0;
+            m_adjacency[v2, v1] = 0;
+        }
+    }
+}
---
+++ Source/UnitTests/Structures/SimpleGraphTests.cs
@@ -0,0 +1,39 @@
+﻿using AlgorithmsDataStructures2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Structures.Trees
+{
+    [TestClass]
+    public class SimpleGraphTests
+    {
+        [TestMethod]
+        public void SimpleGraphCornel()
+        {
+            var graph = new SimpleGraph(3);
+            graph.AddVertex(5);
+            graph.AddVertex(10);
+
+            Assert.AreEqual(5, graph.vertex[0].Value);
+            Assert.AreEqual(10, graph.vertex[1].Value);
+
+            Assert.IsFalse(graph.IsEdge(0, 1));
+
+            graph.AddEdge(0, 1);
+
+            Assert.IsTrue(graph.IsEdge(0, 1));
+
+            graph.AddVertex(15);
+
+            Assert.IsFalse(graph.IsEdge(1, 2));
+            
+            graph.AddEdge(1, 2);
+
+            Assert.IsTrue(graph.IsEdge(1, 2));
+
+            graph.RemoveEdge(1, 2);
+
+            Assert.IsTrue(graph.IsEdge(0, 1));
+            Assert.IsFalse(graph.IsEdge(1, 2));
+        }
+    }
+}
